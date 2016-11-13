a14076cbd963865904b8b1dc68c35fbc79007dcd
jianhong92 <zenboyz@hotmail.com>
2016-11-13 22:47:34 +0800

    able to download to downloads folder and add dropbox del in expire

--- src/main/java/nFileHandler/Download.java
+++ src/main/java/nFileHandler/Download.java
@@ -1,5 +1,7 @@
 package nFileHandler;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
@@ -8,13 +10,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JFileChooser;
 
+import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxEntry;
+import com.dropbox.core.DbxRequestConfig;
+
+import nConstants.DropboxSettings;
 import nDatabase.DBAccess;
 import nUtillities.AESCipher;
 import nUtillities.Logger;
@@ -43,19 +52,23 @@ public class Download extends HttpServlet {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		System.out.println("download servlet called");
+		response.setContentType("text/html");
 				
 		int fileID = Integer.parseInt(request.getParameter("fileID"));
+		String fileName = "";
 		
+	     
 		try{
 			connection = DBAccess.getInstance().openDB();
 			// Get password for selected user account based on given username
-			preparedStatement = connection.prepareStatement("SELECT file_path, file_salt, file_iv FROM files WHERE file_ID = ?");
+			preparedStatement = connection.prepareStatement("SELECT file_path, file_salt, file_iv, file_name FROM files WHERE file_ID = ?");
 			preparedStatement.setInt(1, fileID);
 			ResultSet rset = preparedStatement.executeQuery();
 			byte[] encryptedPath = null;
 			byte[] fileIv = null;
 			byte[] fileSalt = null;
 			while(rset.next()){
+				fileName = rset.getString("file_name");
 				encryptedPath = rset.getBytes("file_path");
 				fileIv = rset.getBytes("file_iv");
 				fileSalt = rset.getBytes("file_salt");
@@ -66,11 +79,35 @@ public class Download extends HttpServlet {
 			
 			String decryptedString = AESCipher.DecryptString(encryptedPath, fileIv, fileSalt);
 			System.out.println("decrypted string: "+ decryptedString);
-//			
+			
+			DbxRequestConfig config = new DbxRequestConfig("FreshDrive", Locale.getDefault().toString());
+			//
+			// // access token for the dropbox account. may need to encrypt this
+			String accessToken = "-TcOHePlr9AAAAAAAAAACMWGsYvDXPTDcThy6nM8r0hwG-Mz5cEqtDxcDygkg9i3";
+			
+			//
+			System.out.println("Connect to dropbox");
+			DbxClient client;
+			client = new DbxClient(config, accessToken);
+//			client.delete("/test/clone.txt");
+			String home = System.getProperty("user.home");
+//			File file = new File(home+"/Downloads/" + fileName);
+			FileOutputStream outputStream = new FileOutputStream(home+"/Downloads/" + fileName);
+	        try {
+	            DbxEntry.File downloadedFile = client.getFile(decryptedString, null,
+	                outputStream);
+	            System.out.println("Metadata: " + downloadedFile.toString());
+	        } finally {
+	            outputStream.close();
+	        }
+			
+			out.println("File has been downloaded");
 		}catch (SQLException e) {
 			Logger.getInstance().PrintError("download sql() ", e.toString());
+			out.println("Download Fail");
 		} catch (Exception e) {
 			Logger.getInstance().PrintError("download exception() ", e.toString());
+			out.println("Download Fail");
 		}
 		
 		
--- src/main/java/nFileHandler/ExpiryFileRemove.java
+++ src/main/java/nFileHandler/ExpiryFileRemove.java
@@ -12,6 +12,7 @@ import com.dropbox.core.DbxRequestConfig;
 import nConstants.Constants;
 import nConstants.DropboxSettings;
 import nDatabase.DBAccess;
+import nUtillities.AESCipher;
 import nUtillities.Logger;
 
 public class ExpiryFileRemove {
@@ -23,33 +24,51 @@ public class ExpiryFileRemove {
 	
 	//Check File Dates to Delete
 		public static void checkFileExpire(){
+			String fileName = "";
+			DbxRequestConfig config = new DbxRequestConfig("FreshDrive", Locale.getDefault().toString());
+			System.out.println("Connect to dropbox");
+			DbxClient client;
+			client = new DbxClient(config, dropboxSettings.getAccessToken());
 			try{
 				connection = DBAccess.getInstance().openDB();
 				
 				//Get All file_id of the Files that is before current dates
 				preparedStatement = connection.prepareStatement( Constants.getDELETE_FILEIDS() );
-				String findFileQuery = "SELECT file_name FROM files WHERE file_expireOn < current_date()";
+				String findFileQuery = "SELECT file_name, file_path, file_iv, file_salt FROM files WHERE file_expireOn < current_date()";
 				findFileStatement = connection.prepareStatement(findFileQuery);
 				ResultSet rset = findFileStatement.executeQuery();
-				String[] fileQueryArray = null;
+				byte[] encryptedPath = null;
+				byte[] fileIv = null;
+				byte[] fileSalt = null;
+//				String[] fileQueryArray = null;
 				while(rset.next()){
-					System.out.println("Expire result set: " + rset.getString(1));
+					fileName = rset.getString("file_name");
+					encryptedPath = rset.getBytes("file_path");
+					fileIv = rset.getBytes("file_iv");
+					fileSalt = rset.getBytes("file_salt");
+					System.out.println("Encrypted path: "+encryptedPath);
+					System.out.println("Encrypted IV: "+fileIv);
+					System.out.println("Encrypted Salt: "+fileSalt);
+//					System.out.println("Expire result set: " + rset.getString(1));
+					String decryptedString = AESCipher.DecryptString(encryptedPath, fileIv, fileSalt);
+					client.delete(decryptedString);
 				}
+				
+//				System.out.println("decrypted string: "+ decryptedString);
 				int rowsDel = preparedStatement.executeUpdate();
 				
 				System.out.println("No. of rows Deleted: " + rowsDel + " at " + new java.util.Date());	
-				DbxRequestConfig config = new DbxRequestConfig("FreshDrive", Locale.getDefault().toString());
+				
 				//
 				// // access token for the dropbox account. may need to encrypt this
 //				String accessToken = "-TcOHePlr9AAAAAAAAAACMWGsYvDXPTDcThy6nM8r0hwG-Mz5cEqtDxcDygkg9i3";
 				//
-				System.out.println("Connect to dropbox");
-				DbxClient client;
 				
 				
-				client = new DbxClient(config, dropboxSettings.getAccessToken());			
 				
 				DBAccess.getInstance().closeDB();
+				
+				
 				
 				
 				
