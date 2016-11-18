3015514a28a5a52753435fafbaf9efb77a331d7f
WenJieChew <wenjie.chew@gmail.com>
2016-11-18 17:37:28 +0800

    adjusted exercutor shutdown

--- src/main/java/nLogin/Login.java
+++ src/main/java/nLogin/Login.java
@@ -152,6 +152,7 @@ public class Login extends HttpServlet {
 		executorService = Executors.newScheduledThreadPool(1);
 		executorService.schedule(() -> {
 			expireOTP(account.getUsername());
+			executorService.shutdown();
 		}, 180L, TimeUnit.SECONDS);
 	}
 
