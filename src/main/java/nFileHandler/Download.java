8172c5b3c9947275649e0e1a2b9a0ac945c89b3a
Josey <josey@live.com.sg>
2016-11-24 01:10:59 +0800

    Check user permission before downloading

--- src/main/java/nFileHandler/Download.java
+++ src/main/java/nFileHandler/Download.java
@@ -31,6 +31,7 @@ import nLogin.Validate;
 import nObjectModel.FileModel;
 import nUtillities.AESCipher;
 import nUtillities.Log;
+import nUtillities.ShareFile;
 
 
 /**
@@ -59,56 +60,74 @@ public class Download extends HttpServlet {
 		response.setContentType("text/html");
 
 		out = response.getWriter();		
+		String username = request.getParameter("username");
+		int fileID = Integer.parseInt(request.getParameter("fileID"));
 
 		//Check User Validity
-		if(Validate.verifyToken(request.getParameter("usertoken")  , request.getParameter("username")) == 1 ){
-			
-			FileModel fileModel = new FileModel();			
-			fileModel.setFileID( Integer.parseInt( request.getParameter("fileID") ) );
-			try{
-				
-				connection = DBAccess.getInstance().openDB();
-				//Get File Path to download from database to download from Dropbox
-				if( getFilePath(fileModel) ){
+		if(Validate.verifyToken(request.getParameter("usertoken")  , username) == 1 ){
+			//Get the userID using the username
+			String[] userInfo = ShareFile.validateUser(username);
+			//If user exists, set the userID else return error message
+			if (userInfo != null){
+				int userID = Integer.parseInt(userInfo[0]);
+				//Check the sharing permissions to ensure that user has permission to file before allowing them to download
+				if (ShareFile.validateUserPermission(userID, fileID) == 1){
+					FileModel fileModel = new FileModel();			
+					fileModel.setFileID(fileID);
 					
-					//Decrypting of Path
-					fileModel.setFilePath( AESCipher.DecryptString( fileModel.getPathByte() , fileModel.getIvByte(), fileModel.getSaltByte() ));
-					
-					//Set FileOutputStream to setup temporary location to store the file 
-		 			FileOutputStream outputStream = new FileOutputStream( Constants.getFilePathLocation() + fileModel.getFileName() );
-		 			
-		 			//Download physical File from File Server (Dropbox)
-		 			DbxEntry.File downloadedFile = client.getFile(fileModel.getFilePath(), null, outputStream);
-		 			outputStream.close();
-		 			
-		 			//Get File from the temporary location
-		 			file = new File(Constants.getFilePathLocation() + fileModel.getFileName() );
-		 			
-		 			//parse file into bytes 
-		            byte[] data = Files.readAllBytes( Paths.get(file.getAbsolutePath()) );
-		            //send the bytes to the client
-		            out.println(Arrays.toString(data));
-		            Log.log("Download Process| "+ request.getParameter("username") + " downloaded " + fileModel.getFileName() + ", size :" +fileModel.getFileLength() + "bytes");
+					try{
+						connection = DBAccess.getInstance().openDB();
+						//Get File Path to download from database to download from Dropbox
+						if( getFilePath(fileModel) ){
+							
+							//Decrypting of Path
+							fileModel.setFilePath( AESCipher.DecryptString( fileModel.getPathByte() , fileModel.getIvByte(), fileModel.getSaltByte() ));
+							
+							//Set FileOutputStream to setup temporary location to store the file 
+				 			FileOutputStream outputStream = new FileOutputStream( Constants.getFilePathLocation() + fileModel.getFileName() );
+				 			
+				 			//Download physical File from File Server (Dropbox)
+				 			DbxEntry.File downloadedFile = client.getFile(fileModel.getFilePath(), null, outputStream);
+				 			outputStream.close();
+				 			
+				 			//Get File from the temporary location
+				 			file = new File(Constants.getFilePathLocation() + fileModel.getFileName() );
+				 			
+				 			//parse file into bytes 
+				            byte[] data = Files.readAllBytes( Paths.get(file.getAbsolutePath()) );
+				            //send the bytes to the client
+				            out.println(Arrays.toString(data));
+				            Log.log("Download Process| "+ username + " downloaded " + fileModel.getFileName() + ", size :" +fileModel.getFileLength() + "bytes");
+						}
+						
+						rs.close();
+						preparedStatement.close();
+						connection.close();
+						
+					}catch (SQLException e) {
+						e.printStackTrace();
+						out.println("Download Fail");
+					}catch (Exception e) {
+						e.printStackTrace();
+						out.println("Download Fail");
+					}finally {
+						//This is to delete the physical file in the temp folder
+						if(file.exists()){
+			            	file.delete();
+		 	            }
+		
+					}
+				}else{
+					System.out.println("permission denied");
+					out.println("permission-denied");
 				}
-				
-				rs.close();
-				preparedStatement.close();
-				connection.close();
-				
-			}catch (SQLException e) {
-				e.printStackTrace();
-				out.println("Download Fail");
-			}catch (Exception e) {
-				e.printStackTrace();
-				out.println("Download Fail");
-			}finally {
-				//This is to delete the physical file in the temp folder
-				if(file.exists()){
-	            	file.delete();
- 	            }
-
 			}
-		}else{
+			else {
+				System.out.println("invalid user");
+				out.println("invalid-user");
+			}
+		}
+		else {
 			System.out.println("token fail");
 			out.println("unverified-token");
 		}
